245f823d6bf0ed20d04270f3efe2adb9b4d3e948
tanaka <lemo.nade.engineer.room@gmail.com>
2024-01-16 17:51:04 +0900

    ターゲット名を変更

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
-      name: "swift-fraction",
-      targets: ["swift-fraction"])
+      name: "Fraction",
+      targets: ["Fraction"])
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "swift-fraction"),
+      name: "Fraction"),
     .testTarget(
-      name: "swift-fractionTests",
-      dependencies: ["swift-fraction"]),
+      name: "FractionTests",
+      dependencies: ["Fraction"]),
   ]
 )
